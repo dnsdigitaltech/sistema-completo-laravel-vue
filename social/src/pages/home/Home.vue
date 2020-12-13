6d2292682a780d369ce8c8215945d294191488a2
Davi Bernardo <dnssites@gmail.com>
2020-12-13 15:16:05 -0300

    Configurando o projeto Vue

---
+++ social/src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+      Social
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nome',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
