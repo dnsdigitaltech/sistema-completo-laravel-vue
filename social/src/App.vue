81399a46dae31b5061da4305a36c5cd516cfd53c
Davi Bernardo <dnssites@gmail.com>
2020-12-14 14:20:57 -0300

    Preparando o menu lateral

--- social/src/App.vue
+++ social/src/App.vue
@@ -7,7 +7,20 @@
       <div class="container">
         <div class="row">
           <grid-vue tamanho="4">
-            Menu
+            <card-menu-vue>
+              <div class="row valign-wrapper">
+                <grid-vue tamanho="8">
+                  <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                </grid-vue>
+                <grid-vue tamanho="8">
+                  <h5>Maria</h5>
+                  <span class="black-text">Add the "circle" class to it to make it appear circular.</span>
+                </grid-vue>
+              </div>
+            </card-menu-vue>
+            <card-menu-vue>
+              <h3>Teste</h3>
+            </card-menu-vue>
           </grid-vue>
           <grid-vue tamanho="8">
             <router-view/>
@@ -26,16 +39,19 @@
 
 <script>
 
-  import GridVue from '@/components/layouts/GridVue'
-  import NavBar from '@/components/layouts/NavBar'
-  import FooterVue from '@/components/layouts/FooterVue'
+ 
+  import NavBar from '@/components/layouts/NavBar.vue'
+  import FooterVue from '@/components/layouts/FooterVue.vue'
+  import GridVue from '@/components/layouts/GridVue.vue'
+  import CardMenuVue from '@/components/layouts/CardMenuVue.vue'
 
   export default {
     name: 'App',
     components: {
       NavBar,
       FooterVue,
-      GridVue
+      GridVue,
+      CardMenuVue
     }
   }
 </script>
