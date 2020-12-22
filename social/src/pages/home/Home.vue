02a8a38fa3435ba216722b47116ec4b11a064efe
Davi Bernardo <dnssites@gmail.com>
2020-12-22 17:10:33 -0300

    Campo para publicar conteudos

--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,30 +1,38 @@
 <template>
-  <div>
-      <card-conteudo-vue 
+  <span>
+
+    <div class="row">
+      <grid-vue class="input-field" tamanho="12">
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+        <label for="textarea1">O que está acontecendo?</label>
+      </grid-vue>
+      <p>
+        <grid-vue class="btn waves-effect waves-light" tamanho="2 offset-s10">Publicar</grid-vue>
+      </p>
+    </div>
+    <card-conteudo-vue 
       perfil = "https://materializecss.com/images/yuna.jpg" 
       nome="Maria Silva" 
-      data="22/12/20 15:00">
-      
+      data="22/12/20 15:00">      
       <card-detalhe-vue
-      img = "https://materializecss.com/images/sample-1.jpg"
-      titulo = "título dinamico"
-      txt = "sdv ksbd vksdb vbsd vbs dbv sdv snd vnsd vnsd vn sdnv snd vnsd vnsdvnmsd vn sdonv sdn vosnd vnsd vnsd vsn"
-      ></card-detalhe-vue>
-
-      </card-conteudo-vue>
-
-      
-  </div>
+        img = "https://materializecss.com/images/sample-1.jpg"
+        titulo = "título dinamico"
+        txt = "sdv ksbd vksdb vbsd vbs dbv sdv snd vnsd vnsd vn sdnv snd vnsd vnsdvnmsd vn sdonv sdn vosnd vnsd vnsd vsn"
+      />
+    </card-conteudo-vue>
+  </span>
 </template>
 
 <script>
   import CardConteudoVue from '@/components/social/CardConteudoVue.vue'
   import CardDetalheVue from '@/components/social/CardDetalheVue.vue'
+  import GridVue from '@/components/layouts/GridVue.vue'
   export default {
     name: 'Nome',
     components: {
       CardConteudoVue,
-      CardDetalheVue
+      CardDetalheVue,
+      GridVue
     },
     data () {
       return {
