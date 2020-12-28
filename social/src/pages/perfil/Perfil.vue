f7e9c9dfbda6a3bc8c7233f0e36814f8e448312d
Davi Bernardo <dnssites@gmail.com>
2020-12-28 16:38:58 -0300

    Tela de Perfil - Parte 2

--- social/src/pages/perfil/Perfil.vue
+++ social/src/pages/perfil/Perfil.vue
@@ -7,10 +7,24 @@
       <h2>Perfil</h2>
       <input type="text" placeholder="Nome" v-model="name">
       <input type="email" placeholder="E-mail" v-model="email">
+    
+      <div class = "row">
+        <div class = "file-field input-field">
+          <div class = "btn">
+              <span>IMAGEM</span>
+              <input type = "file" />
+          </div>
+          
+          <div class = "file-path-wrapper">
+              <input class = "file-path validate" type = "text"
+                placeholder = "Adicionar foto" />
+          </div>
+        </div>
+      </div>
+
       <input type="password" placeholder="Senha" v-model="password">
       <input type="password" placeholder="Confirme sua senha" v-model="password_confirmation">
-      <button class="btn" v-on:click="cadastro()">Enviar</button> 
-      <router-link :to="'/login'" class="btn orange">Já tenho conta</router-link>  
+      <button class="btn" v-on:click="perfil()">Atualizar</button>  
     </span>
   </site-template>
 </template>
@@ -25,16 +39,25 @@
     },
     data () {
       return {
+        usuario:false,
         name:'',
         email:'', 
         password:'',
         password_confirmation:''
       }
     },
+    created() {
+      let usuarioAux = sessionStorage.getItem('usuario');
+      if(usuarioAux){
+        this.usuario = JSON.parse(usuarioAux)        
+        this.name = this.usuario.name
+        this.email = this.usuario.email
+      }
+    },
     methods: {
-      cadastro(){
+      perfil(){
         console.log('ok')
-        axios.post(`http://127.0.0.1:8000/api/cadastro`,{
+        axios.post(`http://127.0.0.1:8000/api/perfil`,{
           name: this.name,
           email: this.email,
           password: this.password,
