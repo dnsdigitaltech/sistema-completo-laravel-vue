ee2569ed85839b6171c845e0cfe869c60a3482d8
Davi Bernardo <dnssites@gmail.com>
2021-01-04 11:07:50 -0300

    Modelo como Adicionar Conteudos

--- sistema-api-laravel8/routes/api.php
+++ sistema-api-laravel8/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SocialController;
+use App\Models\User;
+use App\Models\Conteudo;
+use App\Models\Comentario;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,6 +25,7 @@ Route::group([
 ], function ($router) {
     Route::post('/cadastro', [SocialController::class, 'cadastro']);
     Route::post('/login', [SocialController::class, 'login']);
+
 });
 
 //////////////////////////PERFIL//////////////////////////
@@ -33,4 +37,19 @@ Route::group([
 
 Route::middleware('auth:api')->get('/usuario', function (Request $request) {
     return $request->user();
+});
+
+//////////////////////////TESTES//////////////////////////
+Route::get('/testes', function(){
+    //pegar o primeiro user
+    $user = User::find(1);
+    /*//salvando o conteudo por user
+    $user->conteudos()->create([
+        'titulo' => 'Conteudo 3',
+        'texto' => 'Aqui o texto',
+        'imagem' => 'Url da imagem',
+        'link' => 'Link',
+        'data' => '2021-01-04',
+    ]);*/
+    return $user->conteudos;
 });
