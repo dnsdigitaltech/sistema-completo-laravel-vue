104ab09a5e4a32821619331d9c76903a62fce387
Davi Bernardo <dnssites@gmail.com>
2021-01-04 17:45:10 -0300

    Formulario para publicar conteudo

--- social/src/components/social/PublicarConteudoVue.vue
+++ social/src/components/social/PublicarConteudoVue.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="row">
       <grid-vue class="input-field" tamanho="12">
-        <textarea  v-model="conteudo" class="materialize-textarea"></textarea>
+        <input type="text" v-model="conteudo.titulo">
+        <textarea v-if="conteudo.titulo" placeholder="Conteúdo" v-model="conteudo.texto" class="materialize-textarea"></textarea>
+        <input type="text" v-if="conteudo.titulo && conteudo.texto" placeholder="Link" v-model="conteudo.link">
+        <input type="text" v-if="conteudo.titulo && conteudo.texto" placeholder="Url da Imagem" v-model="conteudo.imagem">
         <label>O que está acontecendo?</label>
       </grid-vue>
       <p>
-        <grid-vue v-if="conteudo" class="btn waves-effect waves-light" tamanho="2 offset-s10">Publicar</grid-vue>
+        <grid-vue v-if="conteudo.titulo && conteudo.texto" class="btn waves-effect waves-light" tamanho="2 offset-s10">Publicar</grid-vue>
       </p>
     </div>
 </template>
@@ -17,7 +20,12 @@
         props: [''],        
         data() {
             return {
-                conteudo: ''
+                conteudo: {
+                  titulo:'',
+                  texto:'',
+                  link:'',
+                  imagem:''
+                  }
             }
         },
         components: {
