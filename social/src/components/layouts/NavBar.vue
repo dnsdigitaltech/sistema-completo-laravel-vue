889b112ba6f8f167b5987b43c330080f13cf2786
Davi Bernardo <dnssites@gmail.com>
2021-01-05 13:55:12 -0300

    Introducao ao VUEX

--- social/src/components/layouts/NavBar.vue
+++ social/src/components/layouts/NavBar.vue
@@ -2,7 +2,7 @@
     <div>
         <nav :class="cor || 'blue'">
             <div class="nav-wrapper container">
-                <router-link :to="url || '/'" class="brand-logo">{{logo || 'Social'}}</router-link>
+                <router-link :to="url || '/'" class="brand-logo">{{logo || 'Social'}} - {{$store.getters.getNome}}</router-link>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <slot/>
                 </ul>
@@ -19,6 +19,9 @@ export default {
         return {
             
         }
+    },
+    created() {
+        this.$store.commit('setNome', 'Outro Nome')
     },
 }
 </script>
